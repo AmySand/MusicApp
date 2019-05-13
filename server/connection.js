43266bf5eb4077f5b38e177a5093c5b29755ac09
Amy Sand <amy.sand@midlandu.edu>
2019-05-13 16:31:55 -0500

    trying to get the database to connect

--- server/connection.js
+++ server/connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
+// require("./config")
 let pool = mysql.createPool({
     connectionLimit: 10,
-    host: "process.env.hostname",
-    database: "process.env.database",
-    password: "process.env.password",
-    port: "process.env.PORT",
-    user: "process.env.username",
+    host: process.env.hostname,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.dbport,
+    user: process.env.username,
     
     
     
